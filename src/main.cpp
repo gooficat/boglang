2363e3519ba2a8e8a33c4b8175f6d7da01685774
Hasan Sabri <hsabri2007@gmail.com>
2025-10-30 11:58:31 +0000

    better parsing

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,32 @@
 #include <algorithm>
 
 
-enum TokenType {
-	lit,
-	fun,
-	ret,
+
+std::vector<std::string> TOKENS = {
+	"ret",
+	"{",
+	"}",
+	";",
+	"fun",
+};
+enum TOKTYPES {
+	RET = 0,
+	BRACEL,
+	BRACER,
+	SEMI,
+	FUN,
+	INDENT,
+	LIT,
+	LABEL
 };
 
 struct Token {
-	TokenType type;
+	TOKTYPES type;
 	std::string value;
+	Token(TOKTYPES type, const std::string& value) : type(type), value(value) {}
 };
+
+
 
 int main (int argc, char *argv[])
 {
@@ -30,63 +46,89 @@ int main (int argc, char *argv[])
 	std::string outpath = argc > 1 ? argv[2] : "C:/Projects/boglang/code/hello.asm";
 	
 	std::ifstream in(argc > 1 ? argv[1] : "C:/Projects/boglang/code/hello.glp");
+	std::string content( (std::istreambuf_iterator<char>(in) ),
+    (std::istreambuf_iterator<char>() ) );
+
 	std::ofstream out(outpath);
 	assert(in);
 	assert(out);
-
-	std::string content(
-		(std::istreambuf_iterator<char>(in)),
-		std::istreambuf_iterator<char>()
-	);
-
 	out.flush();
 	
-
-	std::replace(content.begin(), content.end(), '\n', ' ');
-	std::replace(content.begin(), content.end(), '\t', ' ');
-
-	std::stringstream strm(content);
-
-	std::vector<std::string> words;
-	
-	std::string interm;
-	while (getline(strm, interm, ' ')) {
-		words.emplace_back(interm);
-	}
-
-
-	in.close();
-
-	std::vector<Token> tokens;
+	char c;
 
 	std::string buf = "";
-	for (int i = 0; i != words.size(); i++) {
-		auto& word = words[i];
-		if (word == "" || word == "") continue;
-		std::cout << word;
-		std::string kw = word.substr(0, 3);
-		if( kw == "fun") {
-			// std::cout << word << " " << words[i+1] << std::endl;
-			tokens.emplace_back(TokenType::fun, words[i+1]);
+	std::vector<Token> tokens;
+
+	for (int s = 0; s != content.length(); s++) {
+		c = content[s];
+		if (std::isalpha(c)) {
+			bool matching = true;
+			int i = 0;
+			int j;
+			for (i = 0; i != TOKENS.size(); i++) {
+				matching = true;
+				for (j = 0; j != TOKENS[i].length(); j++) {
+					char t = TOKENS[i][j];
+					c = content[s + j];
+					if (c == t) {
+						// std::cout << c << " is still matching " << TOKENS[i] << "\n";
+					}
+					else {
+						matching = false;
+						// std::cout << "does not match " << c << " " << t << "\n";;
+						break;
+					}
+				}
+				if (matching) {
+					tokens.emplace_back(static_cast<TOKTYPES>(i), "");
+					// std::cout << "It's a match! " << c << " " << TOKENS[i] << "\n";
+					if (s >= content.length()) goto escape;
+					break;
+				}
+			}
+			if (!matching) {
+				buf = "";
+				while (s < content.length() && std::isalpha(c)) {
+					buf += c;
+					s++;
+					c = content[s];
+				}
+				tokens.emplace_back(static_cast<TOKTYPES>(LABEL), buf);
+				if (s >= content.length()) goto escape;
+			}
+			else {
+				s += j;
+			}
 		}
-		else if (kw == "ret") {
-			// std::cout << word << " " << words[i+1] << std::endl;
-			tokens.emplace_back(TokenType::ret, words[i+1]);
+		else if (std::isdigit(c)) {
+			buf = "";
+			while (s < content.length() && std::isdigit(c)) {
+				buf += c;
+				s++;
+				c = content[s];
+			}
+			tokens.emplace_back(static_cast<TOKTYPES>(LIT), buf);
+			if (s >= content.length()) goto escape;
 		}
 	}
-	std::cout << std::endl;
+	escape:
+	in.close();
 
+	for (auto& tok : tokens) {
+		std::cout << "token of type " << tok.type << " with value " << tok.value << std::endl;
+	}
 	
 	std::string ASM = "\
 global _start\n";
 
-	for (auto& tok : tokens) {
+	for (int i = 0; i != tokens.size(); i++) {
+		Token& tok = tokens[i];
 		switch (tok.type) {
-		case fun:
-			ASM += tok.value + ":\n";
+		case FUN:
+			ASM += tok.value + " " + tokens[i+1].value + ":\n";
 			break;
-		case ret:
-			ASM += "mov eax, " + tok.value + "\n";
+		case RET:
+			ASM += "mov eax, " + tokens[i + 1].value + "\n";
 			ASM += "ret\n";
 			break;
 		default:
